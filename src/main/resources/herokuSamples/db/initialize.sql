0fecde5afe6ad6b05ce089be19922bbfbf472d97
k-shunji <k-shunji@flect.co.jp>
2014-06-27 15:32:37 +0900

    Remove resource

--- src/main/resources/herokuSamples/db/initialize.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE hello_test(
-  name     VARCHAR(100) NOT NULL,
-  message  VARCHAR(100) NOT NULL
-);
-
-insert into hello_test (name, message) values('Jane', 'Hello!');
-insert into hello_test (name, message) values('Taro', 'こんにちは!');
-insert into hello_test (name, message) values('Miro', 'Guten Tag!');
-
