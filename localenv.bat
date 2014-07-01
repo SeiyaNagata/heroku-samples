995cc2a5d607e9cc876d9f8b0d55d42cbc065815
k-shunji <k-shunji@flect.co.jp>
2014-07-01 14:32:46 +0900

    Add REDISCLOUD_URL to localenv.bat

--- localenv.bat
+++ localenv.bat
@@ -1,3 +1,4 @@
 set DATABASE_URL=jdbc:h2:mem:test;DB_CLOSE_DELAY=-1
 for /f "usebackq tokens=*" %%i in (`heroku config:get SENDGRID_USERNAME`) do @set SENDGRID_USERNAME=%%i
 for /f "usebackq tokens=*" %%i in (`heroku config:get SENDGRID_PASSWORD`) do @set SENDGRID_PASSWORD=%%i
+for /f "usebackq tokens=*" %%i in (`heroku config:get REDISCLOUD_URL`) do @set REDISCLOUD_URL=%%i
