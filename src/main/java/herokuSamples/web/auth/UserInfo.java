eb61ce98e5df3b030af3f747965885ada5bc8aac
k-shunji <k-shunji@flect.co.jp>
2014-07-24 20:27:59 +0900

    Add connection to UserInfo

--- src/main/java/herokuSamples/web/auth/UserInfo.java
+++ src/main/java/herokuSamples/web/auth/UserInfo.java
@@ -1,6 +1,7 @@
 package herokuSamples.web.auth;
 
 import java.util.Map;
+import java.util.List;
 import com.google.gson.Gson;
 import java.lang.reflect.Type;
 import com.google.gson.reflect.TypeToken;
@@ -30,4 +31,28 @@ public class UserInfo {
 	public String getPictureUrl() { return getValueAsString("picture");}
 	public String getGivenName() { return getValueAsString("given_name");}
 	public String getFamilyName() { return getValueAsString("family_name");}
+
+	public Map<String, Object> getIdentityInfo() {
+		Object o = this.map.get("identities");
+		if (o == null) {
+			return null;
+		} else if (o instanceof Map) {
+			return (Map<String, Object>)o;
+		} else if (o instanceof List) {
+			List list = (List)o;
+			return list.size() == 0 ? null : (Map<String, Object>)list.get(0);
+		} else {
+			throw new IllegalStateException(o.toString());
+		}
+
+	}
+
+	public String getConnection() { 
+		Map<String, Object> identity = getIdentityInfo();
+		if (identity == null) {
+			return "";
+		}
+		String ret = (String)identity.get("connection");
+		return ret == null ? "" : ret;
+	}
 }
